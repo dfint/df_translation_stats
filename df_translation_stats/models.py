28de6220f139018c6ed4d88d7fdb1d1b64df2cb6
insolor <2442833+insolor@users.noreply.github.com>
2025-08-07 18:06:19 +0300

    Split ResourceLanguageStats.id into model ResoruceInfo

--- df_translation_stats/models.py
+++ df_translation_stats/models.py
@@ -8,19 +8,27 @@ class Attributes(BaseModel):
     last_translation_update: datetime | None = None
 
 
+class ResoruceInfo(BaseModel):
+    organization: str
+    project: str
+    resource: str
+    language_code: str
+
+
 class ResourceLanguageStats(BaseModel):
     id: str
     attributes: Attributes
 
     @computed_field
     @property
-    def language_code(self) -> str:
-        return self.id.split(":")[-1]
-
-    @computed_field
-    @property
-    def resource_code(self) -> str:
-        return self.id.split(":")[-3]
+    def resource_info(self) -> ResoruceInfo:
+        _, organization, _, project, _, resource, _, language_code = self.id.split(":")
+        return ResoruceInfo(
+            organization=organization,
+            project=project,
+            resource=resource,
+            language_code=language_code,
+        )
 
 
 class TranslationStats(BaseModel):
