8d943680e643e3275aa0a38dc20b3482c36764a5
insolor <2442833+insolor@users.noreply.github.com>
2025-08-07 21:21:32 +0300

    Move more code into quickchart.py

--- df_translation_stats/main.py
+++ df_translation_stats/main.py
@@ -2,19 +2,18 @@ from __future__ import annotations
 
 import os
 from collections import defaultdict
-from dataclasses import dataclass
 from pathlib import Path
-from typing import TYPE_CHECKING, Any, NewType
+from typing import TYPE_CHECKING
 
 from dotenv import load_dotenv
 from langcodes import Language
 from loguru import logger
 
+from df_translation_stats.models import Dataset, LanguageName
 from df_translation_stats.quickchart import get_chart
 from df_translation_stats.transifex import get_translation_stats
 
 if TYPE_CHECKING:
-    from collections.abc import Iterable
 
     from df_translation_stats.models import TranslationStats
 
@@ -22,49 +21,6 @@ load_dotenv()
 
 DEFAULT_WIDTH = 600
 DEFAULT_LINE_HEIGHT = 14
-
-LanguageName = NewType("LanguageName", str)
-ResourceName = NewType("ResourceName", str)
-
-
-def filter_languages_by_minmal_translation_count(
-    languages: Iterable[LanguageName],
-    count_by_language: dict[LanguageName, int],
-    minimal_count: int,
-) -> list[LanguageName]:
-    return [language for language in languages if count_by_language[language] > minimal_count]
-
-
-@dataclass
-class Dataset:
-    data: dict[ResourceName, dict[LanguageName, float]]
-    languages: list[LanguageName]
-    total_lines: int
-
-    @property
-    def resources(self) -> list[ResourceName]:
-        return list(self.data.keys())
-
-    def with_languages(self, languages: list[LanguageName]) -> Dataset:
-        return Dataset(data=self.data, languages=languages, total_lines=self.total_lines)
-
-    def get_count_by_languages(self) -> dict[LanguageName, int]:
-        return {language: sum(item[language] for item in self.data.values()) for language in self.languages}
-
-    def sort_languages(self) -> None:
-        count_by_language = self.get_count_by_languages()
-        self.languages = sorted(
-            self.languages,
-            key=lambda language: (-count_by_language[language], language),
-        )
-
-    def with_minimal_translation_percent(self, minimal_percent: float) -> Dataset:
-        languages = filter_languages_by_minmal_translation_count(
-            languages=self.languages,
-            count_by_language=self.get_count_by_languages(),
-            minimal_count=minimal_percent / 100 * self.total_lines,
-        )
-        return self.with_languages(languages)
 
 
 def prepare_dataset(raw_data: TranslationStats) -> Dataset:
@@ -88,45 +44,14 @@ def prepare_dataset(raw_data: TranslationStats) -> Dataset:
     )
 
 
-def prepare_chart_data(dataset: Dataset) -> dict[str, Any]:
-    datasets = [
-        dict(
-            label=resource,
-            data=[lines[label] for label in dataset.languages],
-        )
-        for resource, lines in dataset.data.items()
-    ]
-
-    return dict(
-        type="horizontalBar",
-        data=dict(labels=dataset.languages, datasets=datasets),
-        options=dict(
-            scales=dict(
-                yAxes=[dict(stacked=True)],
-                xAxes=[
-                    dict(
-                        stacked=True,
-                        ticks=dict(
-                            beginAtZero=True,
-                            max=dataset.total_lines,
-                            stepSize=10000,
-                        ),
-                    ),
-                ],
-            ),
-        ),
-    )
-
-
 def generate_diagram(
     dataset: Dataset,
     width: int,
     height: int,
     output_file: Path,
 ) -> None:
-    chart_data = prepare_chart_data(dataset)
     file_format = output_file.suffix[1:]
-    chart = get_chart(chart_data, file_format=file_format, width=width, height=height)
+    chart = get_chart(dataset, file_format=file_format, width=width, height=height)
     output_file.write_bytes(chart)
     logger.info(f"{output_file.name} chart file is saved")
 
--- df_translation_stats/quickchart.py
+++ df_translation_stats/quickchart.py
@@ -1,22 +1,57 @@
 from __future__ import annotations
 
 from types import SimpleNamespace
-from typing import Any
+from typing import TYPE_CHECKING, Any
 
 import httpx
 from scour.scour import scourString as scour_string  # noqa: N813
+
+if TYPE_CHECKING:
+    from df_translation_stats.models import Dataset
 
 
 def minify_svg(data: bytes) -> bytes:
     return scour_string(data.decode("utf-8"), options=SimpleNamespace(strip_ids=True, shorten_ids=True)).encode("utf-8")
 
 
+def prepare_chart_data(dataset: Dataset) -> dict[str, Any]:
+    datasets = [
+        dict(
+            label=resource,
+            data=[lines[label] for label in dataset.languages],
+        )
+        for resource, lines in dataset.data.items()
+    ]
+
+    return dict(
+        type="horizontalBar",
+        data=dict(labels=dataset.languages, datasets=datasets),
+        options=dict(
+            scales=dict(
+                yAxes=[dict(stacked=True)],
+                xAxes=[
+                    dict(
+                        stacked=True,
+                        ticks=dict(
+                            beginAtZero=True,
+                            max=dataset.total_lines,
+                            stepSize=10000,
+                        ),
+                    ),
+                ],
+            ),
+        ),
+    )
+
+
 def get_chart(
-    chart_data: dict[str, Any],
+    dataset: Dataset,
     file_format: str = "png",
     width: int = 800,
     height: int = 800,
 ) -> bytes:
+    chart_data = prepare_chart_data(dataset)
+
     url = "https://quickchart.io/chart"
     payload = dict(
         width=width,
